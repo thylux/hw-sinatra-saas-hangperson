437276c0ee7f4e424957982bb1dd2bcfeec7d693
thylux <thylux@gmail.com>
2015-11-07 19:55:05 +0000

    Part 1 Finished

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,7 +2,10 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
-
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
   # Get a word from remote "random word" service
 
   # def initialize()
@@ -10,6 +13,45 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  def guess(letter)
+    raise ArgumentError if !letter || letter == '' || (letter =~ /^[a-z]/i) == nil
+    
+    my_letter = letter.downcase
+    valid = @word.downcase.include? my_letter
+    
+    if valid
+      if !@guesses.include? my_letter
+        @guesses += my_letter
+      else
+        valid = false
+      end
+    else
+      if !@wrong_guesses.include? my_letter
+        @wrong_guesses += my_letter
+        valid = true
+      end
+    end
+    
+    valid
+  end
+  
+  def word_with_guesses
+    g = (@guesses == '' ? '1' : @guesses)
+    @word.gsub(/[^#{g}]/i, '-')
+  end
+  
+  def check_win_or_lose
+    if @word == word_with_guesses
+      :win
+    elsif @wrong_guesses.length >= 7
+      :lose
+    else
+      :play
+    end
   end
 
   def self.get_random_word
