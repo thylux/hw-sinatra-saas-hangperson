ba4a905a74a3e22e8776e7860dc3862cb7a1dea5
thylux <thylux@gmail.com>
2015-11-07 21:03:30 +0000

    Final

--- app.rb
+++ app.rb
@@ -39,7 +39,14 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    begin
+      if !@game.guess(letter)
+        flash[:message] = "You have already used that letter."
+      end
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
+    
     redirect '/show'
   end
   
@@ -49,18 +56,30 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    status = @game.check_win_or_lose
+    if status == :play
+      erb :show # You may change/remove this line
+    else
+      erb @game.check_win_or_lose
+    end
   end
   
   get '/win' do
-    ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    status = @game.check_win_or_lose
+    if status == :play
+      erb :show # You may change/remove this line
+    else
+      erb @game.check_win_or_lose
+    end
   end
   
   get '/lose' do
-    ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    status = @game.check_win_or_lose
+    if status == :play
+      erb :show # You may change/remove this line
+    else
+      erb @game.check_win_or_lose
+    end
   end
   
 end
